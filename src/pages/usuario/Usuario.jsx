0fbd432fe44df19e22c0f178bbd26a69af6950ef
Jessica Trindade Nascimento de Brito <141635580+jessicatrindadeads@users.noreply.github.com>
2026-08-19 18:00:44 -0300

    fix: use dados de contato estáveis no perfil

--- src/pages/usuario/Usuario.jsx
+++ src/pages/usuario/Usuario.jsx
@@ -1,48 +1,50 @@
-import S from "./usuario.module.scss";
-import Mapa from "../../assets/img/mapa.png";
-import Carta from "../../assets/img/carta.png";
 import Calendario from "../../assets/img/calendario.png";
+import Carta from "../../assets/img/carta.png";
+import Mapa from "../../assets/img/mapa.png";
+import S from "./usuario.module.scss";
 
-export default function Perfil() {
+export default function Usuario() {
   return (
     <main className={S.container}>
       <section className={S.card}>
         <div className={S.imagemPerfil}>
           <img
-            src="https://media.licdn.com/dms/image/v2/D4D03AQGzzpIcOvxMYQ/profile-displayphoto-scale_400_400/B4DZfHUOo.GYAk-/0/1751395659198?e=1773273600&v=beta&t=lwRvPwz7z0fsA76MdvpKyOAlTJp2ZwwB0qU6QCmvCzE"
-            alt="Foto do usuário"
+            src="https://avatars.githubusercontent.com/u/141635580?v=4"
+            alt="Jéssica Trindade"
           />
         </div>
 
         <div className={S.info}>
           <h1>Jéssica Trindade</h1>
-
-          <p className={S.status}>Voluntária Ativa</p>
-
+          <p className={S.status}>Voluntária ativa</p>
           <p className={S.descricao}>
             Apaixonada por fazer a diferença na comunidade. Acredito que
-            pequenas ações podem transformar vidas e estou sempre em busca
-            de novas oportunidades para ajudar.
+            pequenas ações podem transformar vidas e estou sempre em busca de
+            novas oportunidades para ajudar.
           </p>
 
           <ul className={S.informacoes}>
             <li>
-              <img src={Mapa} alt="Ícone de mapa" />
+              <img src={Mapa} alt="" />
               Guarujá, SP
             </li>
-
             <li>
-              <img src={Carta} alt="Ícone de carta" />
-              jessica.trindade@gmail.com
+              <img src={Carta} alt="" />
+              <a
+                href="https://www.linkedin.com/in/jessicatrindadeads"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Contato profissional
+              </a>
             </li>
-
             <li>
-              <img src={Calendario} alt="Ícone de calendário" />
-              Membro desde Janeiro 2022
+              <img src={Calendario} alt="" />
+              Membro desde janeiro de 2022
             </li>
           </ul>
 
-          <ul className={S.tags}>
+          <ul className={S.tags} aria-label="Áreas de interesse">
             <li>Educação</li>
             <li>Meio Ambiente</li>
             <li>Assistência Social</li>
